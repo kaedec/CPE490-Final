a6207e735240a97c03563beb1988aaf071b6d72c
Katelyn <kaecharbonneau@gmail.com>
2017-12-12 14:44:20 -0500

    Documenting, organizing
    n/m to say

--- CppProject/main.cpp
+++ CppProject/main.cpp
@@ -2,6 +2,7 @@
  * CPE 490 Midterm
  *
  * Author: Katelyn Charbonneau
+ * Date: 12/14/2017
  *
  */
 
@@ -14,8 +15,9 @@
 using namespace exploringBB;
 using namespace std;
 
-#define LDR_PATH "/sys/bus/iio/devices/iio:device0/in_voltage"
+#define LDR_PATH "/sys/bus/iio/devices/iio:device0/in_voltage" // used for both LDR and TMP sensors
 
+//This function reads the value from the requested file (either AIN0 or AIN1)
 int readAnalog(int number){
    stringstream ss;
    ss << LDR_PATH << number << "_raw";
@@ -26,6 +28,7 @@ int readAnalog(int number){
    return number;
 }
 
+//This function converts the TMP reading to Celcius
 float getTemperature(int adc_value){
    float cur_voltage = adc_value * (1.80f/4096.0f);
    float diff_degreesC = (cur_voltage-0.75f)/0.01f;
@@ -34,26 +37,32 @@ float getTemperature(int adc_value){
 
 int main(int argc, char* argv[]){
 	/*
-	 * pin 17 = slot 23 = gpio49
-	 * analog0 = slot 39
+	 * GPIO Locations:
+	 * LED: pin 17 = slot 23 = gpio49
+	 * LDR: analog0 = slot 39
+	 * TMP: analog1 = slot 40
 	 */
-   GPIO ledGPIO(49);
-
-   // Basic Output - Flash the LED 10 times, once per second
+	
+   GPIO ledGPIO(49); // Using GPIO class
    ledGPIO.setDirection(OUTPUT);
 
-   int LDRvalue = readAnalog(0);
+   int LDRvalue = readAnalog(0); //Get reading from LDR
 
-   if(LDRvalue < 2000){
-	   ledGPIO.setValue(HIGH);
-	   
+   if(LDRvalue < 2000){ //2000 was chosen as the breakpoint for the environment that testing was completed in
+	   ledGPIO.setValue(HIGH); //Turn the indicator LED on
+		
+			/*
+			 * This reads the value from the TMP sensor, converts it to Celcius, and then writes
+			 * the LDR reading, TMP reading, and Celcius conversion to the output.
+			 * This output will be grabbed by the Python script.
+			 */
 		   int TMPvalue = readAnalog(1);
 		   float temperature = getTemperature(TMPvalue);
 		   cout << LDRvalue << "," << TMPvalue << "," << temperature << endl;
 	   
    }
 	   else{
-	   ledGPIO.setValue(LOW);
+	   ledGPIO.setValue(LOW); //Turn the indicator LED off
 	   
    }
    return 0;
--- Python/sensor_data_parser.py
+++ Python/sensor_data_parser.py
@@ -1,8 +1,16 @@
-import csv
-import os
+#CPE 460 Final Project
+#Author: Katelyn Charbonneau
+#Date: 12/14/2017
 
+import csv	#for parsing .csv file
+import os	#for executing bash commands
+
+#note that the significant weakness to this project is that due to the usage of Node-RED, absolute file paths had to be used
+
+#execute the C++ program and write the output to a file
 os.system("/home/debian/Katelyn_Charbonneau_IoT/CPE490-Final/CppProject/Debug/CPE490-Final >> /home/debian/Katelyn_Charbonneau_IoT/CPE490-Final/Python/raw_data.csv")
 
+#open the file and parse data into a list
 with open('/home/debian/Katelyn_Charbonneau_IoT/CPE490-Final/Python/raw_data.csv','rb') as f:
 	reader = csv.reader(f)
 	read_list = list(reader)
@@ -11,23 +19,35 @@ s1 = []
 s2 = []
 s3 = []
 
+#parse through the data list created above
+#place LDR Raw data into S1
+#place TMP Raw data into S2
+#place Celcius data into S3
 for i in range(-1,len(read_list)-1):
 	s1.append(read_list[i][0])
 	s2.append(read_list[i][1])
 	s3.append(read_list[i][2])
 
-print s1
-print s2
-print s3
+#debugging - uncomment any of these lines to see its contents
+#print s1
+#print s2
+#print s3
 
+#Convert data from string to int, or float, as required
 s1 = map(int, s1)
 s2 = map(int, s2)
 s3 = map(float, s3)
 
+#make new lists containing the average, minimum, and maximum values for each reading
+#results[0] = average
+#results[1] = minimum
+#results[2] = maximum
 s1_results = [float(format(sum(map(float, s1))/len(s1), '.1f')), min(s1), max(s1)]
 s2_results = [float(format(sum(map(float, s2))/len(s2), '.1f')), min(s2), max(s2)]
 s3_results = [float(format(sum(map(float, s3))/len(s3), '.1f')), min(s3), max(s3)]
 
+#Open the ouput text file
+#Print the data to it in a formatted manner
 f = open('/home/debian/Katelyn_Charbonneau_IoT/CPE490-Final/report.txt','w+')
 f.write('\t\t\tSummary of Data from Sensors\n')
 f.write('\t\tAverage\t\tMinimum Reading\t\tMaximum Reading\n')
